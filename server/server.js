493915134dbb6a9de75b9755e367d6d81dfef55b
beuleu <billyklotz789@gmail.com>
2023-03-05 15:42:16 +0100

    get one table

--- server/server.js
+++ server/server.js
@@ -4,13 +4,11 @@ const data    = require("./data.json")
 
 const app = express()
 app.use(express.json())
-app.get('/', (req, res) => {
-  console.log("hello world")
-})
+
+//get all tables
 app.get("/tables", async(req, res) => {
   res.send(data)
 })
-
 
 //create a new table
 app.post('/tables', async(req, res) => {
@@ -19,6 +17,7 @@ app.post('/tables', async(req, res) => {
 
   let arrayOfId = data.map(d => d.id);
   console.log("ids are ", arrayOfId)
+  newData.expenses = []
   newData.id = data.length >= 1? Math.max(...arrayOfId) + 1: 1
   fs.readFile("./data.json", "utf8", (err, data) => {
     const json = JSON.parse(data)
@@ -27,6 +26,15 @@ app.post('/tables', async(req, res) => {
     fs.writeFile("./data.json", JSON.stringify(json), (err) => {
       if ( err ) throw err
     })
+  })
+})
+
+//get one table
+app.get("/tables/:id", async(req, res) => {
+  fs.readFile("./data.json", "utf8", (err, data) => {
+    const table = JSON.parse(data).find(t => t.id == req.params.id)
+    if (table) return res.send(table)
+    res.sendStatus(404)
   })
 })
 
