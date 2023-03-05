49c33d7911010a697d38bcbe66d239d646c052a4
beuleu <billyklotz789@gmail.com>
2023-03-05 15:55:45 +0100

    table route refactor

---
+++ server/routes/tablesRoute.js
@@ -0,0 +1,39 @@
+const tablesRouter = require("express").Router();
+
+const data = require("../data.json");
+const fs   = require("fs");
+//get all tables
+tablesRouter.get("/", async(req, res) => {
+  res.send(data)
+})
+
+//create a new table
+tablesRouter.post('/', async(req, res) => {
+  console.log("request body", req.body)
+  const newData = req.body
+
+  let arrayOfId = data.map(d => d.id);
+  console.log("ids are ", arrayOfId)
+  newData.expenses = []
+  newData.id = data.length >= 1? Math.max(...arrayOfId) + 1: 1
+  fs.readFile("./data.json", "utf8", (err, data) => {
+    const json = JSON.parse(data)
+    json.push(req.body)
+    console.log(json)
+    fs.writeFile("./data.json", JSON.stringify(json), (err) => {
+      if ( err ) throw err
+      res.sendStatus(200)
+    })
+  })
+})
+
+//get one table
+tablesRouter.get("/:id", async(req, res) => {
+  fs.readFile("./data.json", "utf8", (err, data) => {
+    const table = JSON.parse(data).find(t => t.id == req.params.id)
+    if (table) return res.send(table)
+    res.sendStatus(404)
+  })
+})
+
+module.exports = tablesRouter
--- server/server.js
+++ server/server.js
@@ -1,43 +1,13 @@
 const express = require('express');
-const fs      = require('fs');
-const data    = require("./data.json")
 
 const app = express()
+const port = process.env.PORT || 4000
+
+const tablesRouter = require('./routes/tablesRoute');
 app.use(express.json())
+app.use("/tables", tablesRouter);
 
-//get all tables
-app.get("/tables", async(req, res) => {
-  res.send(data)
-})
 
-//create a new table
-app.post('/tables', async(req, res) => {
-  console.log("request body", req.body)
-  const newData = req.body
-
-  let arrayOfId = data.map(d => d.id);
-  console.log("ids are ", arrayOfId)
-  newData.expenses = []
-  newData.id = data.length >= 1? Math.max(...arrayOfId) + 1: 1
-  fs.readFile("./data.json", "utf8", (err, data) => {
-    const json = JSON.parse(data)
-    json.push(req.body)
-    console.log(json)
-    fs.writeFile("./data.json", JSON.stringify(json), (err) => {
-      if ( err ) throw err
-    })
-  })
-})
-
-//get one table
-app.get("/tables/:id", async(req, res) => {
-  fs.readFile("./data.json", "utf8", (err, data) => {
-    const table = JSON.parse(data).find(t => t.id == req.params.id)
-    if (table) return res.send(table)
-    res.sendStatus(404)
-  })
-})
-
-app.listen(4000, () => {
-  console.log('port 4000 lets goooooo!!!')
+app.listen(port, () => {
+  console.log(`port ${port} lets goooooo!!!`)
 })
