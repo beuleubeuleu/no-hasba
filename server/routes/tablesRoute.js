4c0ba6c7ce98c5bf39707b37ad78cfc6bddef7c4
beuleu <billyklotz789@gmail.com>
2023-03-05 17:10:30 +0100

    get expenses of a table

--- server/routes/tablesRoute.js
+++ server/routes/tablesRoute.js
@@ -4,7 +4,9 @@ const data = require("../data.json");
 const fs   = require("fs");
 //get all tables
 tablesRouter.get("/", async(req, res) => {
-  res.send(data)
+  fs.readFile("./data.json", "utf8", (err, data) => {
+    res.send(data)
+  })
 })
 
 //create a new table
@@ -15,11 +17,10 @@ tablesRouter.post('/', async(req, res) => {
   let arrayOfId = data.map(d => d.id);
   console.log("ids are ", arrayOfId)
   newData.expenses = []
-  newData.id = data.length >= 1? Math.max(...arrayOfId) + 1: 1
+  newData.id       = data.length >= 1 ? Math.max(...arrayOfId) + 1 : 1
   fs.readFile("./data.json", "utf8", (err, data) => {
     const json = JSON.parse(data)
     json.push(req.body)
-    console.log(json)
     fs.writeFile("./data.json", JSON.stringify(json), (err) => {
       if ( err ) throw err
       res.sendStatus(200)
@@ -31,7 +32,16 @@ tablesRouter.post('/', async(req, res) => {
 tablesRouter.get("/:id", async(req, res) => {
   fs.readFile("./data.json", "utf8", (err, data) => {
     const table = JSON.parse(data).find(t => t.id == req.params.id)
-    if (table) return res.send(table)
+    if ( table ) return res.send(table)
+    res.sendStatus(404)
+  })
+})
+
+//get expenses of a table
+tablesRouter.get("/:id/expenses", async(req, res) => {
+  fs.readFile("./data.json", "utf8", (err, data) => {
+    const table = JSON.parse(data).find(t => t.id == req.params.id)
+    if ( table ) return res.send(table.expenses)
     res.sendStatus(404)
   })
 })
