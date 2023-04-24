79213b1c80ccc678b8df03638c23114db2a0aa14
beuleu <billyklotz789@gmail.com>
2023-04-24 16:36:14 +0200

    mysql2 get all groups + get group by id

--- server/src/dependency/TrigroupSql.js
+++ server/src/dependency/TrigroupSql.js
@@ -1,8 +1,26 @@
 const InterfaceTrigroup = require("../class/InterfaceTrigroup")
+const mysql = require('mysql2/promise');
 
 class TrigroupSql extends InterfaceTrigroup {
-  async getGroups(){
+  constructor(config) {
+    super();
+    this.config = config;
+  }
 
+  async getGroups(){
+    const connection = await mysql.createConnection(this.config);
+    const [rows, fields] = await connection.execute('SELECT * FROM trigroup');
+    await connection.end();
+    console.log(rows, fields)
+    return rows;
+  }
+
+  async getGroupById(idGroup){
+    const connection = await mysql.createConnection(this.config);
+    const [rows, fields] = await connection.execute('SELECT * FROM trigroup WHERE trigroup_id = ' + idGroup);
+    await connection.end();
+    console.log(rows)
+    return rows;
   }
 }
 
--- server/src/dependency/abstractFactory.js
+++ server/src/dependency/abstractFactory.js
@@ -1,8 +1,8 @@
 const TrigroupJson = require("./TrigroupJson");
-const TrigroupSql = require("./TrigroupSql")
+const TrigroupSql  = require("./TrigroupSql")
 
 exports.abstractFactory = (tech) => {
-  if (tech === "json") return new TrigroupJson
-  if (tech === "sql") return new TrigroupSql
+  if ( tech === "json" ) return new TrigroupJson
+  if ( tech === "sql" ) return new TrigroupSql({ host: "localhost", database:"tricount_clone" ,port: 8889, user: "root", password: "root" })
   throw new Error("Invalid")
 }
